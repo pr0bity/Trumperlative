eb79e4a0c1dde26a120e565588277b4047cfc3f8
pr0bity <valerie.palermo@gmail.com>
2016-01-06 18:56:14 -0500

    Commented out web components. Command-line only now

--- isms.py
+++ isms.py
@@ -1,20 +1,18 @@
 # Trump superlative generator
 
 import random
-from flask import Flask, request, url_for
-
 
 #---- Clauses ----#
 
 # Intro
-intro = ["", "And by the way, ", "", "Believe me: ", "", "As I have been saying, "] 
+intro = ["", "And by the way... ", "", "Believe me: ", "", "As I have been saying... "] 
 
 # Route a:
-a0 = ["I am ", "I'm ", "I will be ", "I'll be ", "As of this moment, I am ", "I'm going to be "] 
+a0 = ["I am ", "I'm ", "I will be ", "I'll be ", "As of this moment, I am ", "I'm going to be ",] 
 
 ## Route a1
-a1 = [["really ", "so ", "tremendously ", "incredibly ", "very much ", "unbelievably "],
-			["rich ", "awesome ", "healthy ", "handsome ", "good for women ", "militaristic "]]
+a1 = [["really ", "so ", "tremendously ", "incredibly ", "very ", "unbelievably "],
+			["rich. ", "awesome. ", "healthy. ", "handsome. ", "good for women. ", "militaristic. "]]
 
 ## Route a2
 a2 = [["the best ", "the most humble ", "the smartest ", "the most fantastic ", "the most successful ", "the greatest "],
@@ -22,56 +20,52 @@ a2 = [["the best ", "the most humble ", "the smartest ", "the most fantastic ", 
 		  ["in the history of the United States.", "ever.", ".", "that God ever created." , "there is.", "in the world."]]
 
 # Route b:
-b = [["I'd ", "I would ", "We will ", "I'll ", "We'll ", "We are going to "],
-		["hit ISIS ", "protect America ", "create jobs ", "build walls ", "win ", "fight back " ],
-		["so hard, ", "so much, ", "so easily, " , "with strength and stamina -- ", "with so much energy, ", " so fabulously," ],
-		[" your head would spin.", " it will make a grand statement.", " it will be unrivaled." , " it'll be a home run!" , " it will be iconic." ," and that is 100 percent true."]]
+b = [["I'd ", "I would ", "we will ", "I'll ", "we'll ", "we are going to "],
+		["hit ISIS ", "protect America ", "create jobs ", "build walls ", "win ", "fight back ", "serve our veterans " ],
+		["so hard, ", "so much, ", "so easily, " , "with strength and stamina -- ", "with so much energy, ", "so fabulously," ],
+		["your head would spin. ", "it will make a grand statement. ", "it will be unrivaled. " , "it'll be a home run! " , "it will be iconic. " ,"and that is 100 percent true. "]]
 
+# Route c:
+#c = [["You know who cheats at golf? ", "I greatly respect ", "I'm very proud of ", "One of my best friends is ", "I'm not a fan of "
 # Closing
-conclusion = [ " I'll show you!", "", " And yes, it is my hair.", "", " It's high-priority.", " I'm very proud of it."]
+conclusion = [ "I'll show you!", "", "And yes, it is my hair.", "", "It's high-priority.", " I'm very proud of it."]
 
 #------------------#
 
-app = Flask(__name__)
-app.secret_key = 'This is really unique and secret'
 
-@app.route('/')
-def main_page():
-    return """
-        <p>TRUMP SUPERLATIVE GENERATOR</p>
-        <form method="POST" action="%s">
-		<button type="submit"> The Donald says...</button>
-		</form>
-        """ % (url_for('generate_ism'),)
+TrumpLative = ""
 
-@app.route('/trumperlative', methods=['POST'])
-def generate_ism():
-	TrumpLative = ""
+# Use a Trump opener
+TrumpLative = TrumpLative + random.choice(intro)
 
-	# Use a Trump opener
-	TrumpLative = TrumpLative + random.choice(intro)
+# Pick a route
+route = random.randrange(1)
+if(route == 0):# Go with what Trump is
+	TrumpLative = TrumpLative + random.choice(a0)
 
-	# Flip a coin
-	if(random.randrange(1)):# Go with what Trump is
-		TrumpLative = TrumpLative + random.choice(a0)
-	
-		# Flip another coin
-		if (random.randrange(1)):
-			for line in range(2):
-				TrumpLative = TrumpLative + random.choice(a1[line])
-		else:
-			for line in range(3):
-				TrumpLative = TrumpLative + random.choice(a2[line])
-	
-	
-	else:	# Go with what Trump will do
-		for line in range(4):
-			TrumpLative = TrumpLative + random.choice(b[line])
+	# Flip another coin
+	coin = random.randrange(1)
+	if (coin == 0):
+		for line in range(2):
+			TrumpLative = TrumpLative + random.choice(a1[line])
+	else:
+		for line in range(3):
+			TrumpLative = TrumpLative + random.choice(a2[line])
 
-	# Wrap it up
-	TrumpLative = TrumpLative + random.choice(conclusion)
-	 
-	return """ <p>%s</p> """ % (TrumpLative, url_for('main_page'))
+#else if(route == 1):
+#	for line in range(2):
+#		TrumpLative = TrumpLative + 
+
+else:	# Go with what Trump will do
+	for line in range(4):
+		TrumpLative = TrumpLative + random.choice(b[line])
+
+# Wrap it up
+TrumpLative = TrumpLative + random.choice(conclusion)
+
+print(TrumpLative)
+ 
+
 
 
 
